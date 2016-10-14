8c73499d81f39e55ab5b874cd13cf3ad1b068d51
6vBj <hbliuwei123@gmail.com>
2016-10-14 14:36:54 +0800

    Initial Commit

---
+++ SnowDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnowDemo
+//
+//  Created by liuwei on 16/10/14.
+//  Copyright © 2016年 liuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
